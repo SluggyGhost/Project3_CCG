19d2e3431fab4cf73a60a87c2ab78fb8d6bd2096
Josh Acosta <jodacost@ucsc.edu>
2025-05-20 23:00:06 -0700

    split const file into const.lua and config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+
+gameState = {
+  turn = 1,
+  phase = TURN_PHASE.DRAW
+}
+winningScore = 20
+turnLimit = false   -- Off by default
+maxTurns = 0
+numPlayers = 2  -- Two players by default
+
+width = love.graphics.getWidth()
+height = love.graphics.getHeight()
+centerX, centerY = width/2, height/2
+
+cardWidth, cardHeight = 50, 70
---
+++ const.lua
@@ -0,0 +1,15 @@
+
+CARD_STATE = {
+  IDLE = 1,
+  MOUSE_OVER = 2,
+  GRABBED = 3
+}
+
+TURN_PHASE = {
+  DRAW = 1,   -- Draw card
+  MANA = 2,   -- Set Mana
+  PLAY = 3,   -- Play cards
+  REVEAL = 4, -- Reveal cards
+  SCORE = 5,  -- Get score
+  END = 6     -- End of turn
+}
